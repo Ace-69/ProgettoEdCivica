360d7f2c4f15d5bc1043f7ac461829d930a09e11
Ace-69 <francescosomma3@gmail.com>
2023-03-16 13:27:00 +0100

    now theme stays between pages

--- js/theme.js
+++ js/theme.js
@@ -1,9 +1,41 @@
-let i = 0;
+let i = parseInt(getCookie("darkmode"));
+window.onload = function(){
+    i--;
+    theme();
+};
+
+console.log(i);
 let css = document.getElementById('css');
 function theme() {
     i++;
     if(i==2){
         i=0;
         css.href = "css/style.css";
-    } else if(i==1) css.href = "css/style-dark.css";
+    } else if(i==1){
+        css.href = "css/style-dark.css";
+    } 
+    setCookie("darkmode", i, 99);
 }
+
+function setCookie(cname, cvalue, exdays) {
+    const d = new Date();
+    d.setTime(d.getTime() + (exdays*24*60*60*1000));
+    let expires = "expires="+ d.toUTCString();
+    document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+}
+
+function getCookie(cname) {
+    let name = cname + "=";
+    let decodedCookie = decodeURIComponent(document.cookie);
+    let ca = decodedCookie.split(';');
+    for(let i = 0; i <ca.length; i++) {
+        let c = ca[i];
+        while (c.charAt(0) == ' ') {
+            c = c.substring(1);
+        }
+        if (c.indexOf(name) == 0) {
+            return c.substring(name.length, c.length);
+        }
+    }
+    return 0;
+  }
